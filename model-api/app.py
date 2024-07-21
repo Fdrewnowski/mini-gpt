a74b8eb4f3a2958bdd90f02c6eda720c8f29d9b7
Fdrewnowski <f.drewnowski@wp.pl>
2024-07-22 00:51:57 +0200

    * add model download and loading during startup * add model prediction in predict endpoint

--- model-api/app.py
+++ model-api/app.py
@@ -2,6 +2,7 @@
 import os
 from fastapi import Request, FastAPI
 from fastapi.logger import logger
+from huggingface_hub import hf_hub_download
 from llama_cpp import Llama
 
 
@@ -9,15 +10,18 @@ app = FastAPI()
 
 @app.on_event("startup")
 def startup():
-    logger.info('Application start sup')
+    logger.info('Application start up')
     try :
+        file_path = hf_hub_download(repo_id="TheBloke/Llama-2-13B-GGUF", filename="llama-2-13b.Q2_K.gguf")
+
         app.llm = Llama(
-            model_path="./models/7B/llama-model.gguf",
+            model_path=file_path,
             # n_gpu_layers=-1, # Uncomment to use GPU acceleration
             # seed=1337, # Uncomment to set a specific seed
             # n_ctx=2048, # Uncomment to increase the context window
         )
-            
+        print("Model loaded")
+
     except Exception as e:
         print(e)
 
@@ -36,19 +40,19 @@ async def get_body(request: Request):
     request = await request.json()
     if type(request) is list:
         request = request[0]
-
-    model_input = request
-    # model_input = ""Q: Name the planets in the solar system? A: "
+    if type(request) is dict:
+        request = request['data']
 
     print (request)
+    model_input = request
+
 
     output = app.llm(
-            "Q: Name the planets in the solar system? A: ", # Prompt
-            max_tokens=32, # Generate up to 32 tokens, set to None to generate up to the end of the context window
+            model_input, # Prompt
+            max_tokens=20, # Generate up to 32 tokens, set to None to generate up to the end of the context window
             stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
             echo=True # Echo the prompt back in the output
         ) # Generate a completion, can also call create_completion
-    print(output)
     
     return {"predictions": output }
 
